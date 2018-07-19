3c056fc0bd68c1ceaecc95d107902df94b742ad1
windikadm <windikadm3@gmail.com>
2018-07-19 22:39:29 +0700

    first commit

--- app/Book.php
+++ app/Book.php
@@ -6,12 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    $book = new \App\Book;
-
-    $book->nama ='kitabul jami';
-    $book->genre = 'religi';
-    $book->harga = '20000'
-
-    $book->save();
-
+    //	
 }
